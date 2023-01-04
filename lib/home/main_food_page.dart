786209325eaa1c8ed6511ab449466e5bb261878e
lokeshny97 <lokeshny97@gamil.com>
2023-01-04 17:35:23 +0530

    Added : sliding screen practice

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/colors/app_colors.dart';
+import 'package:food_delivery_app/widgets/big_text.dart';
+import 'package:food_delivery_app/widgets/small_text.dart';
+
+import 'food_page_body.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 45,),
+            padding: EdgeInsets.only(left: 20,right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:  [
+                   BigText(text: 'India',color: AppColors.mainColor),
+                    Row(
+                      children: [
+                        SmallText(text: 'Bengaluru',color: Colors.black54,),
+                        const Icon(Icons.arrow_drop_down_rounded)
+                      ],
+                    )
+
+                  ],
+                ),
+                Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color:AppColors.mainColor,
+                  ),
+                  child: const Icon(Icons.search,color: Colors.white,),
+                )
+              ],
+            ),
+          ),
+          const Expanded(child: SingleChildScrollView(child: FoodPageBody()))
+        ],
+      )
+    );
+  }
+}
---
+++ lib/utils/dimension.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class Dimension{
+static double screenHeight = Get.context!.height;
+static double screenWidth = Get.context!.width;
+
+static double pageViewContainer = screenHeight/3.21;
+static double pageTextContainer = screenHeight/5.89;
+static double pageView = screenHeight/2.2;
+
+static double height10 = screenHeight/70.7;
+}
